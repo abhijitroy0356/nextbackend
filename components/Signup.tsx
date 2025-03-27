a535342777158560dfe4acafa5a47ab1b240e9f8
abhijitroy0356 <60839264+abhijitroy0356@users.noreply.github.com>
2025-03-28 03:05:10 +0530

    update next js sign up

---
+++ components/Signup.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export function Signup(){
+    return (
+        <div>
+            <input type="text" placeholder="username" />
+            <input type="text" placeholder="password"/>
+            <button>sign-up</button>
+        </div>
+    )
+}
